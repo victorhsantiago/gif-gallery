1391acfbee8bf7e810425bfab8c63f0e672d7728
Victor Santiago <victorhsantiago3@gmail.com>
2025-03-20 10:14:37 +0100

    chore: configure project settings and add styled-components dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@models': path.resolve(__dirname, './src/models'),
+      '@services': path.resolve(__dirname, './src/services'),
+    },
+  },
 })
